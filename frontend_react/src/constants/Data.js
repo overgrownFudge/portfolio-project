037795a96a56245d6222dc723e04770344b5402e
overgrownFudge <overgrownfudge@gmail.com>
2022-07-30 18:22:28 -0400

    set up folder structure and components
    assets,
    container,
    constants,
    components

---
+++ frontend_react/src/constants/Data.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        "id": "1",
+        "title": "Infinix INBOOK",
+        "description": "Infinix Inbook X1 Ci3 10th 8GB...",
+        "project_link": "https://github.com/adrianhajdin/project_professional_portfolio/blob/master/src/container/Work/Work.jsx",
+        "tag": ["UI/UX", "All"],
+        "image": "https://dummyjson.com/image/i/products/9/thumbnail.jpg"
+
+    },
+    {
+        "id": "2",
+        "title": "Microsoft Surface Laptop 4",
+        "description": "Style and speed. Stand out on ...",
+        "project_link": "https://github.com/adrianhajdin/project_professional_portfolio/blob/master/src/container/Work/Work.jsx",
+        "tag": ["React JS","All"],
+        "image": "https://dummyjson.com/image/i/products/8/thumbnail.jpg"
+    },
+    {
+        "id": "3",
+        "title": "perfume Oil",
+        "description": "Mega Discount, Impression of A...",
+        "project_link": "https://github.com/adrianhajdin/project_professional_portfolio/blob/master/src/container/Work/Work.jsx",
+        "tag": ["Mobile App", "All"],
+        "image": "https://dummyjson.com/image/i/products/11/thumbnail.jpg"
+    }
+
+]
