356a8c77ccfe94ce66839b69ba06b6fbd9d25db9
overgrownFudge <overgrownfudge@gmail.com>
2022-07-30 18:18:40 -0400

    replaced react src file

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { About, Header, Footer, Work, Skills, Testimonial } from './container/index.js'
+import { Navbar } from './components'
+import './App.scss'
+
+export default function App() {
+    return (
+        <div className='app'>
+            <Navbar />
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    )
 }
-
-export default App;
