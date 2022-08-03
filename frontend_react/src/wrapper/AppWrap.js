5f195b1f60dd49df15a02a103fe68c3c3438d0c1
overgrownFudge <overgrownfudge@gmail.com>
2022-08-03 14:49:41 -0400

    created wrapper component

---
+++ frontend_react/src/wrapper/AppWrap.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function AppWrap(Component, idName, classNames) {
+    return function () {
+        return (
+            <div id={idName} className={`app__container ${classNames}`}>
+                <SocialMedia />
+                <div className="app__wrapper app__flex">
+                    <Component/>
+
+                    <div className="copyright">
+                        <p className="p-text">@2022 ISMAIL</p>
+                        <p className="p-text">All rights reserved</p>
+                    </div>
+                </div>
+                <NavigationDots active={idName} />
+            </div>
+        );
+    }
+};
