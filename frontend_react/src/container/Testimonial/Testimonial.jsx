d15a3d32811fc9142156b4c4f233620e97de4b28
overgrownFudge <overgrownfudge@gmail.com>
2022-08-06 12:26:24 -0400

    completed testimonial component

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+
+import { AppWrap } from '../../wrapper'
+
+
+////VVVVVVVVthese are dummy files VVVVVVVV
+import { testimonial } from '../../constants'
+import { brand } from '../../constants'
+///\/\/\/\these are dummy files /\/\/\/\
 
 import './Testimonial.scss'
 
-export default function Testimonial(){
+function Testimonial() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+
+    setTestimonials(testimonial);
+    setBrands(brand);
+
+  }, []);
+
   return (
-    <div>Testimonial</div>
-  )
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={testimonials[currentIndex].image} alt={testimonials[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand.id}
+          >
+            <img src={brand.image} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 }
+
+export default AppWrap(Testimonial,'testimonial')
