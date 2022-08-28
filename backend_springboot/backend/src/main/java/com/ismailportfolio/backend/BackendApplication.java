853852268b537af9e30e154213057937068409d1
overgrownFudge <overgrownfudge@gmail.com>
2022-08-28 18:23:07 -0400

    replaced backend file

--- backend_springboot/backend/src/main/java/com/ismailportfolio/backend/BackendApplication.java
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.ismailportfolio.API;
+package com.ismailportfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend_springboot/backend/src/test/java/com/ismailportfolio/backend/BackendApplicationTests.java
+++ backend_springboot/backend/src/test/java/com/ismailportfolio/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ismailportfolio.API;
+package com.ismailportfolio.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
