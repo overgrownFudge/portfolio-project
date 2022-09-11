29c914fd8089f6e24bd6f701d357a4ca1476e181
overgrownFudge <overgrownfudge@gmail.com>
2022-09-10 20:49:50 -0400

    added skill service class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/skill/SkillService.java
@@ -0,0 +1,21 @@
+package com.ismailportfolio.backend.skill;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SkillService {
+
+    private final SkillRepository skillRepository;
+
+    @Autowired
+    public SkillService(SkillRepository skillRepository) {
+        this.skillRepository = skillRepository;
+    }
+
+    public List<Skill> getSkill() {
+        return skillRepository.findAll();
+    }
+}
