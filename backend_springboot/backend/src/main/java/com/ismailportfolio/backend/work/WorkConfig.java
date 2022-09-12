4c487d9749c520d6630868fb66f026a8f2a6c2be
overgrownFudge <overgrownfudge@gmail.com>
2022-09-11 21:25:50 -0400

    added work config class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/work/WorkConfig.java
@@ -0,0 +1,28 @@
+package com.ismailportfolio.backend.work;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class WorkConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            WorkRepository repository){
+        return args -> {
+            Work Infinix_INBOOK = new Work(
+                    "Infinix INBOOK",
+                    "Infinix Inbook X1 Ci3 10th 8GB...",
+                    "https://github.com/adrianhajdin/project_professional_portfolio/blob/master/src/container/Work/Work.jsx",
+                    "UI/UX",
+                    "https://dummyjson.com/image/i/products/9/thumbnail.jpg"
+                    );
+            repository.saveAll(
+                    List.of(Infinix_INBOOK)
+            );
+        };
+    }
+}
