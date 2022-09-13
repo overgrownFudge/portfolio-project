d3058410cdb2f99bb67f453d2100679d368c9ff4
overgrownFudge <overgrownfudge@gmail.com>
2022-09-12 20:13:59 -0400

    added brand service class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/brand/BrandService.java
@@ -0,0 +1,21 @@
+package com.ismailportfolio.backend.brand;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BrandService {
+
+    private final BrandRepository brandRepository;
+
+    @Autowired
+    public BrandService(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    public List<Brand> getBrand() {
+        return brandRepository.findAll();
+    }
+}
