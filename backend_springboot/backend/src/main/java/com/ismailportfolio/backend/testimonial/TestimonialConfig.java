c0da918b416f37475e4689032fee6d5e78f27049
overgrownFudge <overgrownfudge@gmail.com>
2022-09-13 19:34:52 -0400

    added testimonial config class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/testimonial/TestimonialConfig.java
@@ -0,0 +1,33 @@
+package com.ismailportfolio.backend.testimonial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class TestimonialConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            TestimonialRepository repository){
+        return args -> {
+            Testimonial Facebook = new Testimonial(
+                    "Sarah",
+                    "Facebook",
+                    "Infinix Inbook X1 Ci3 10th 8GB...",
+                    "https://dummyjson.com/image/i/products/9/thumbnail.jpg"
+            );
+            Testimonial Google = new Testimonial(
+                    "Jason",
+                    "Google",
+                    "Style and speed. Stand out on ...",
+                    "https://dummyjson.com/image/i/products/8/thumbnail.jpg"
+            );
+            repository.saveAll(
+                    List.of(Facebook, Google)
+            );
+        };
+    }
+}
