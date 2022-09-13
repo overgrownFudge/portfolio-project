217b740bbd3b51626cae3ef97bb3ff59015183b4
overgrownFudge <overgrownfudge@gmail.com>
2022-09-12 20:13:51 -0400

    added brand repo class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.ismailportfolio.backend.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository
+        extends JpaRepository<Brand, Long> {
+}
