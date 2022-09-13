e8cca167ec0a8b9dad936d8d7405dded4b4e043c
overgrownFudge <overgrownfudge@gmail.com>
2022-09-12 20:13:32 -0400

    added brand class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/brand/Brand.java
@@ -0,0 +1,68 @@
+package com.ismailportfolio.backend.brand;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Brand {
+    @Id
+    @SequenceGenerator(
+            name = "brand_sequence",
+            sequenceName = "brand_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "brand_sequence"
+    )
+    private Long id;
+    private String name;
+    private String image;
+
+    public Brand() {
+    }
+
+    public Brand(Long id, String name, String image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public Brand(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
