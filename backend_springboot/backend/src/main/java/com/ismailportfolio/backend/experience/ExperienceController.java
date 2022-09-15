aeb830ff482c08abdbbe9cf92061fa633b0fce41
overgrownFudge <overgrownfudge@gmail.com>
2022-09-14 23:08:25 -0400

    added experience controller class

---
+++ backend_springboot/backend/src/main/java/com/ismailportfolio/backend/experience/ExperienceController.java
@@ -0,0 +1,25 @@
+package com.ismailportfolio.backend.experience;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/experience")
+public class ExperienceController {
+
+    private final ExperienceService experienceService;
+
+    @Autowired
+    public ExperienceController(ExperienceService experienceService) {
+        this.experienceService = experienceService;
+    }
+
+    @GetMapping
+    public List<Experience> getExperience() {
+        return experienceService.getExperience();
+    }
+}
