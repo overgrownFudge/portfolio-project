b83b923e051a77468146e769e453eafae7408c00
overgrownFudge <overgrownfudge@gmail.com>
2022-10-04 19:05:39 -0400

    removed extra nav dot

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavigationDots({ active }) {
     return (
         <div className='app__navigation'>
-            {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
+            {['home', 'about', 'work', 'skills', /*'testimonial',*/ 'contact'].map((item, index) => (
                 <a href={`#${item}`}
                     key={item + index}
                     className='app__navigation-dot'
